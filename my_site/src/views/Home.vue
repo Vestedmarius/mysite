6b79388e846d4572a3e7daa431d7c3469922dd7b
TheConsoleGardenMG <45213589+Vestedmarius@users.noreply.github.com>
2020-10-07 10:15:09 +0200

    Update Home.vue
    ie oe

--- my_site/src/views/Home.vue
+++ my_site/src/views/Home.vue
@@ -3,6 +3,7 @@
     <vue-particles style="position:absolute; width:100vw; height: 100vh;" class="mx-n5" linesColor="#ff0000"  :linesWidth="2" color="#ff0000"></vue-particles>
     <div class="pt-15 text-center">
       <h1>TheConsoleGardenMG</h1>
+      <p>ie oe</p>
     </div>
   </div>
 </template>
