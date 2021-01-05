7642c9990ea5cc9c551256af0660a7b2e3a8ef6a
TheConsoleGardenMG <45213589+Vestedmarius@users.noreply.github.com>
2021-01-05 12:04:20 +0100

    home fix
    Fix partikels op home

--- my_site/src/views/Home.vue
+++ my_site/src/views/Home.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="home">
-    <vue-particles style="position:absolute; width:100vw; height: 100vh;" class="mx-n5" linesColor="#ff0000"  :linesWidth="2" color="#ff0000"></vue-particles>
-    <v-card color="rgba(255, 0, 0, 0.0)" class="mx-auto py-15" width="40%" flat>
-      <v-card-title>
-        <h1>TheConsoleGardenMG</h1>
-      </v-card-title>
-    </v-card>
-    <h1 class="text-center">TheConsoleGardenMG</h1>
-    <v-row>
-      <v-col cols="4">
-        <v-card>
-          <v-card-title>aaa</v-card-title>
-          <v-card-text>nu wel?</v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <vue-particles style="position:absolute; width:100vw; height: 100vh;" class="mx-n5" linesColor="#ff0000"  :linesWidth="2" color="#ff0000">
+      <v-card color="rgba(255, 0, 0, 0.0)" class="mx-auto py-15" width="40%" flat>
+        <v-card-title>
+          <h1>TheConsoleGardenMG</h1>
+        </v-card-title>
+      </v-card>
+      <h1 class="text-center">TheConsoleGardenMG</h1>
+      <v-row>
+        <v-col cols="4">
+          <v-card>
+            <v-card-title>aaa</v-card-title>
+            <v-card-text>nu wel?</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </vue-particles>
   </div>
 </template>
 
